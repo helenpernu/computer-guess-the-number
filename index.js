4b1a3239eed1fd1655fc8b4847d7c15417b2f217
Helen <helen.pernu@skola.botkyrka.se>
2020-11-09 16:01:25 +0100

    First commit

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const MAX_NUMBER = 100
 const MIN_NUMBER = 0
 
-let guess
+let guessRange = 50
 
 document.getElementById('too-high-btn').addEventListener('click', function () {
-    // Vad ska hända när man tryckt too high?
+    guessRange /= 2
+    guess += guessRange
+    updateMessage()
+    
 
 })
 
 document.getElementById('too-low-btn').addEventListener('click', function () {
-    // Vad ska hända när man tryckt too low?
+    guessRange /= 2
+    guess += guessRange
+    updateMessage()
     
 })
+
+document.getElementById('is-correct-btn').addEventListener('click', function () {
+    updateMessage()
+    setMessage("Woho! :)")
+})
+
+
+function updateMessage() {
+    setMessage(`Is it ${guess}?`)
+}
 
 
 function setMessage(msg) {
@@ -22,9 +37,9 @@ function start() {
     document.getElementById('before-start').style.display = 'none'
     document.querySelector('main').style.display = 'block'
 
-    guess = 0
+    guess = 50
 
-    // setMessage(`Is it ${guess}?`)
+    setMessage(`Is it ${guess}?`)
 }
 
 document
